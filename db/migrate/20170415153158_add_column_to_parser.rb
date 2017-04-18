92c02aaa7809b20cd74d89b55a009c910aa67f78
Anton Polomkin <antonp@123software.ru>
2017-04-18 14:14:19 +0300

    fix items

--- db/migrate/20170415153158_add_column_to_parser.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnToParser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :parser, :meta_title, :string
-    add_column :parser, :meta_description, :string
-    add_column :parser, :meta_image, :string
-    add_column :parser, :title, :string
-    add_column :parser, :link_news, :string
-  end
-end
