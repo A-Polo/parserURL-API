533f41655b1c6410b48d3c9d44dd811c90756dd0
Anton Polomkin <aap@0814c0003.local>
2017-09-13 19:47:25 +0300

    Add device

---
+++ app/controllers/time_controller.rb
@@ -0,0 +1,19 @@
+class TimeController < ApplicationController
+
+  def time
+    return render_401 unless current_user
+
+    render json: {
+        payload: {
+            time: Time.now.to_i
+        }
+    }
+  end
+
+  protected
+
+  def render_401
+    render json: { errors: ['not authorized'] }, status: 401
+  end
+
+end
