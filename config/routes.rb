54d6e583858e8954c760c005c0ab448be1e86e63
Anton Polomkin <aap@0814c0003.local>
2017-10-09 19:33:27 +0300

    Update devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'links/linkpreview'
+  get 'time/time'
   resources :items, only: [:index,:create]
+  mount_devise_token_auth_for 'User', at: 'auth'
 end
