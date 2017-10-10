cb7d0dd7c2e0b4c97f5bf9e0b4a82fa8a3e4f9bf
Anton Polomkin <aap@0814c0003.local>
2017-10-10 18:46:58 +0300

    Update omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use ActionDispatch::Cookies
 Rails.application.config.middleware.use ActionDispatch::Session::CookieStore
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider 'github', ENV.fetch('github_key'), ENV.fetch('github_secret')
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
